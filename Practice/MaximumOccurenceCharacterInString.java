9f09aad41546ffeb56057255150f9d5680cce7cb
AbhiGupta470 <80680371+AbhiGupta470@users.noreply.github.com>
2025-05-10 23:58:39 +0530

    Create MaximumOccurenceCharacterInString.java

---
+++ Practice/MaximumOccurenceCharacterInString.java
@@ -0,0 +1,39 @@
+// Maximum occurence of a character in a string by HashMap
+// Input : "Shubh"
+// Output: "h"
+
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Main
+{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<str.length(); i++)
+        {
+            if(!map.containsKey(str.charAt(i)))
+            {
+                map.put(str.charAt(i), 1);
+            } else {
+                map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
+            }
+        }
+        
+        int min = Integer.MIN_VALUE;
+        char ch = Character.MIN_VALUE; // this is a new concept
+        for(Character ele: map.keySet())
+        {
+            if(map.get(ele) > min)
+            {
+                min = map.get(ele);
+                ch = ele;
+            }
+            
+        }
+        
+        System.out.print(ch);
+    }
+}
